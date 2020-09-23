43d925ff28f6f5e15e1efbd2138faf4eda1a5b25
Laurel Kelly <laurellkelly@gmail.com>
2020-09-23 14:41:13 -0700

    Create expenseActions.js and incomeActions.js; Update expenseReducer.js and incomeReducer.js to use the actions

--- src/js/components/IncomeEntries/incomeReducer.js
+++ src/js/components/IncomeEntries/incomeReducer.js
@@ -5,5 +5,42 @@ const defaultState = {
 };
 
 export default function IncomeReducer (state = defaultState, action) {
-    return state;
+    const { type, payload } = action;
+
+    switch (type) {
+        // Here in the case of the update description action
+        case 'UPDATE_INCOME_DESCRIPTION': {
+            // we'll return an object
+            return {
+                // with all the previous state
+                ...state,
+                // but overwriting the description
+                description: payload.description
+            };
+        }
+
+        case 'UPDATE_INCOME_AMOUNT': {
+            return {
+                ...state,
+                amount: payload.amount
+            };
+        }
+
+        case 'ADD_INCOME': {
+            const { description, amount } = action.payload;
+            return {
+                description: '',
+                action: '',
+                lineItems: [
+                    // here we have all the previous line items
+                    ...state.lineItems,
+                    // plus a new object
+                    { description, amount }
+                ]
+            };
+        }
+        default: {
+            return state;
+        }
+    }
 }
